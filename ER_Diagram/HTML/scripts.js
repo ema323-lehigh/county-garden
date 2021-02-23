de7ba756a8cfbb421c3e1cf3b13b78b22c68f3cc
Emily Asadoorian <ema323@lehigh.edu>
2021-02-22 22:37:40 -0500

    Fail to implement total participation

--- ER_Diagram/HTML/scripts.js
+++ ER_Diagram/HTML/scripts.js
@@ -5,6 +5,9 @@ window.onload = function () {
         var conn1 = document.getElementById(node.dataset.connects.split(" ")[0]);
         var conn2 = document.getElementById(node.dataset.connects.split(" ")[1]);
         var line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        if (node.dataset.participation.includes("total-1")) {
+            line1.classList += "total";
+        }
         line1.setAttribute('id', node.dataset.connects.split(" ").join("-") + "-1");
         line1.setAttribute('x1', conn1.getBoundingClientRect().left + (conn1.offsetWidth / 2));
         line1.setAttribute('y1', conn1.getBoundingClientRect().top + (conn1.offsetHeight / 2));
@@ -12,6 +15,9 @@ window.onload = function () {
         line1.setAttribute('y2', node.getBoundingClientRect().top + (node.offsetHeight / 2));
         svg.appendChild(line1);
         var line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        if (node.dataset.participation.includes("total-2")) {
+            line2.classList += "total";
+        }
         line2.setAttribute('id', node.dataset.connects.split(" ").join("-") + "-2");
         line2.setAttribute('x1', node.getBoundingClientRect().left + (node.offsetWidth / 2));
         line2.setAttribute('y1', node.getBoundingClientRect().top + (node.offsetHeight / 2));
