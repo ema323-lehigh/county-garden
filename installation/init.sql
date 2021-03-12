3ada095ef147a74325dfd72e74ca656916b5c9d5
Emily Asadoorian <ema323@lehigh.edu>
2021-03-12 16:29:30 -0500

    Add some more tables to the init.sql

--- installation/init.sql
+++ installation/init.sql
@@ -22,10 +22,6 @@ CREATE TABLE customer (
     lastname VARCHAR(50),
     suffix VARCHAR(10),
     birth_date DATE,
-    street VARCHAR(100),
-    city VARCHAR(50),
-    state VARCHAR(2),
-    zipcode NUMBER(5),
     PRIMARY KEY cust_id,
     FOREIGN KEY agent_id REFERENCES agent,
 )
@@ -33,5 +29,42 @@ CREATE TABLE customer (
 CREATE TABLE phone_num (
     numb NUMBER(10),
     kind VARCHAR(10),
+    PRIMARY KEY phone_num,
     FOREIGN KEY cust_id REFERENCES customer,
+)
+
+CREATE TABLE address (
+    street VARCHAR(100),
+    city VARCHAR(50),
+    state VARCHAR(2),
+    zipcode NUMBER(5),
+    PRIMARY KEY (street, city),
+    FOREIGN KEY cust_id REFERENCES customer,
+)
+
+CREATE TABLE contractor (
+    firm_id NUMBER(6),
+    industry VARCHAR(20),
+    name VARCHAR(50),
+    phone VARCHAR(10),
+    PRIMARY KEY firm_id,
+)
+
+CREATE TABLE services (
+    service_date DATE,
+    FOREIGN KEY claim_id REFERENCES claim,
+    FOREIGN KEY firm_id REFERENCES contractor,
+    PRIMARY KEY (claim_id, firm_id),
+)
+
+CREATE TABLE claim (
+    claim_id NUMBER(6),
+    claim_title VARCHAR(50),
+    location VARCHAR(100),
+    description VARCHAR(280),
+    submitted_date DATE,
+    occurred_date DATE,
+    FOREIGN KEY policy_id REFERENCES policy,
+    FOREIGN KEY cust_id REFERENCES customer,
+    FOREIGN KEY adj_id REFERENCES adjuster,
 )
