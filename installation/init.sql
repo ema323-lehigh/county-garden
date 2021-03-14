4d9aa72980984fdb94252e13e1f4e6e1d7d8b2b6
Emily Asadoorian <ema323@lehigh.edu>
2021-03-14 16:49:36 -0400

    Add missing parentheses in init SQL

--- installation/init.sql
+++ installation/init.sql
@@ -4,9 +4,9 @@ CREATE TABLE policy (
     policy_type VARCHAR(20) NOT NULL,
     quoted_price NUMBER(6,2),
     cancelled BOOLEAN NOT NULL,
-    FOREIGN KEY cust_id REFERENCES customer NOT NULL
+    FOREIGN KEY (cust_id) REFERENCES customer NOT NULL
         ON DELETE CASCADE,
-    PRIMARY KEY policy_id,
+    PRIMARY KEY (policy_id),
 )
 
 DROP TABLE item;
@@ -14,9 +14,9 @@ CREATE TABLE item (
     item_id NUMBER(6) NOT NULL,
     category VARCHAR(20) NOT NULL,
     approx_value NUMBER(6,2) NOT NULL,
-    FOREIGN KEY policy_id REFERENCES policy NOT NULL
+    FOREIGN KEY (policy_id) REFERENCES policy NOT NULL
         ON DELETE CASCADE,
-    PRIMARY KEY item_id,
+    PRIMARY KEY (item_id),
 )
 
 DROP TABLE customer;
@@ -27,18 +27,18 @@ CREATE TABLE customer (
     lastname VARCHAR(50) NOT NULL,
     suffix VARCHAR(10),
     birth_date DATE NOT NULL,
-    FOREIGN KEY agent_id REFERENCES agent NOT NULL
+    FOREIGN KEY (agent_id) REFERENCES agent NOT NULL
         ON DELETE CASCADE,
-    PRIMARY KEY cust_id,
+    PRIMARY KEY (cust_id),
 )
 
 DROP TABLE phone_num;
 CREATE TABLE phone_num (
     numb NUMBER(10) NOT NULL,
     kind VARCHAR(10) NOT NULL,
-    FOREIGN KEY cust_id REFERENCES customer NOT NULL
+    FOREIGN KEY (cust_id) REFERENCES customer NOT NULL
         ON DELETE CASCADE,
-    PRIMARY KEY numb,
+    PRIMARY KEY (numb),
 )
 
 DROP TABLE address;
@@ -47,7 +47,7 @@ CREATE TABLE address (
     city VARCHAR(50) NOT NULL,
     state VARCHAR(2) NOT NULL,
     zipcode NUMBER(5) NOT NULL,
-    FOREIGN KEY cust_id REFERENCES customer NOT NULL
+    FOREIGN KEY (cust_id) REFERENCES customer NOT NULL
         ON DELETE CASCADE,
     PRIMARY KEY (street, city),
 )
@@ -58,15 +58,15 @@ CREATE TABLE contractor (
     industry VARCHAR(20) NOT NULL,
     name VARCHAR(50) NOT NULL,
     phone VARCHAR(10) NOT NULL,
-    PRIMARY KEY firm_id,
+    PRIMARY KEY (firm_id),
 )
 
 DROP TABLE services;
 CREATE TABLE services (
     service_date DATE NOT NULL,
-    FOREIGN KEY firm_id REFERENCES contractor NOT NULL,
+    FOREIGN KEY (firm_id) REFERENCES contractor NOT NULL,
         ON DELETE CASCADE,
-    FOREIGN KEY claim_id REFERENCES claim NOT NULL
+    FOREIGN KEY (claim_id) REFERENCES claim NOT NULL
         ON DELETE CASCADE,
     PRIMARY KEY (firm_id, claim_id, service_date),
 )
@@ -79,18 +79,18 @@ CREATE TABLE claim (
     description VARCHAR(280) NOT NULL,
     occurred_date DATE NOT NULL,
     submitted_date DATE NOT NULL,
-    FOREIGN KEY policy_id REFERENCES policy NOT NULL,
+    FOREIGN KEY (policy_id) REFERENCES policy NOT NULL,
         ON DELETE CASCADE,
-    FOREIGN KEY cust_id REFERENCES customer NOT NULL,
+    FOREIGN KEY (cust_id) REFERENCES customer NOT NULL,
         ON DELETE CASCADE,
-    PRIMARY KEY claim_id,
+    PRIMARY KEY (claim_id),
 )
 
 DROP TABLE manages;
 CREATE TABLE manages (
-    FOREIGN KEY claim_id REFERENCES claim NOT NULL,
+    FOREIGN KEY (claim_id) REFERENCES claim NOT NULL,
         ON DELETE CASCADE,
-    FOREIGN KEY adj_id REFERENCES adjuster NOT NULL,
+    FOREIGN KEY (adj_id) REFERENCES adjuster NOT NULL,
         ON DELETE CASCADE,
     PRIMARY KEY (claim_id, adj_id),
 )
@@ -99,7 +99,7 @@ DROP TABLE agent;
 CREATE TABLE agent (
     agent_id NUMBER(6) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    PRIMARY KEY agent_id,
+    PRIMARY KEY (agent_id),
 )
 
 DROP TABLE adjuster;
@@ -107,7 +107,7 @@ CREATE TABLE adjuster (
     adj_id NUMBER(6) NOT NULL,
     name VARCHAR(100) NOT NULL,
     specialty VARCHAR(20),
-    PRIMARY KEY adj_id,
+    PRIMARY KEY (adj_id),
 )
 
 DROP TABLE dependent;
@@ -115,7 +115,7 @@ CREATE TABLE dependent (
     name VARCHAR (100) NOT NULL,
     relationship VARCHAR(20) NOT NULL,
     birth_date DATE NOT NULL,
-    FOREIGN KEY cust_id REFERENCES customer NOT NULL
+    FOREIGN KEY (cust_id) REFERENCES customer NOT NULL
         ON DELETE CASCADE,
     PRIMARY KEY (name, relationship, cust_id),
 )
@@ -125,9 +125,9 @@ CREATE TABLE invoice (
     trans_id NUMBER(6) NOT NULL,
     due_date DATE NOT NULL,
     payment_type VARCHAR(10),
-    FOREIGN KEY policy_id REFERENCES policy NOT NULL
+    FOREIGN KEY (policy_id) REFERENCES policy NOT NULL
         ON DELETE CASCADE,
-    PRIMARY KEY trans_id,
+    PRIMARY KEY (trans_id),
 )
 
 DROP TABLE payment;
@@ -135,7 +135,7 @@ CREATE TABLE payment (
     payment_id NUMBER(6) NOT NULL,
     amount NUMBER(6,2) NOT NULL,
     paid_date DATE NOT NULL,
-    FOREIGN KEY claim_id REFERENCES claim NOT NULL
+    FOREIGN KEY (claim_id) REFERENCES claim NOT NULL
         ON DELETE CASCADE,
-    PRIMARY KEY payment_id,
+    PRIMARY KEY (payment_id),
 )
