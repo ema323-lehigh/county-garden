77a6c04cdf62ac6b12e3daf3b91d46aa37c7f8f9
Emily Asadoorian <ema323@lehigh.edu>
2021-03-14 16:04:00 -0400

    Add NOT NULL constraints

--- installation/init.sql
+++ installation/init.sql
@@ -1,109 +1,109 @@
 DROP TABLE policy;
 CREATE TABLE policy (
-    policy_id NUMBER(6),
-    policy_type VARCHAR(20),
+    policy_id NUMBER(6) NOT NULL,
+    policy_type VARCHAR(20) NOT NULL,
     quoted_price NUMBER(6,2),
-    cancelled BOOLEAN,
-    FOREIGN KEY cust_id REFERENCES customer,
+    cancelled BOOLEAN NOT NULL,
+    FOREIGN KEY cust_id REFERENCES customer NOT NULL,
     PRIMARY KEY policy_id,
 )
 
 DROP TABLE item;
 CREATE TABLE item (
-    item_id NUMBER(6),
-    category VARCHAR(20),
-    approx_value NUMBER(6,2),
-    FOREIGN KEY policy_id REFERENCES policy,
+    item_id NUMBER(6) NOT NULL,
+    category VARCHAR(20) NOT NULL,
+    approx_value NUMBER(6,2) NOT NULL,
+    FOREIGN KEY policy_id REFERENCES policy NOT NULL,
     PRIMARY KEY item_id,
 )
 
 DROP TABLE customer;
 CREATE TABLE customer (
-    cust_id NUMBER(6),
-    firstname VARCHAR(50),
+    cust_id NUMBER(6) NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
     middlename VARCHAR(50),
-    lastname VARCHAR(50),
+    lastname VARCHAR(50) NOT NULL,
     suffix VARCHAR(10),
-    birth_date DATE,
-    FOREIGN KEY agent_id REFERENCES agent,
+    birth_date DATE NOT NULL,
+    FOREIGN KEY agent_id REFERENCES agent NOT NULL,
     PRIMARY KEY cust_id,
 )
 
 DROP TABLE phone_num;
 CREATE TABLE phone_num (
-    numb NUMBER(10),
-    kind VARCHAR(10),
-    FOREIGN KEY cust_id REFERENCES customer,
+    numb NUMBER(10) NOT NULL,
+    kind VARCHAR(10) NOT NULL,
+    FOREIGN KEY cust_id REFERENCES customer NOT NULL,
     PRIMARY KEY numb,
 )
 
 DROP TABLE address;
 CREATE TABLE address (
-    street VARCHAR(100),
-    city VARCHAR(50),
-    state VARCHAR(2),
-    zipcode NUMBER(5),
-    FOREIGN KEY cust_id REFERENCES customer,
+    street VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    state VARCHAR(2) NOT NULL,
+    zipcode NUMBER(5) NOT NULL,
+    FOREIGN KEY cust_id REFERENCES customer NOT NULL,
     PRIMARY KEY (street, city),
 )
 
 DROP TABLE contractor;
 CREATE TABLE contractor (
-    firm_id NUMBER(6),
-    industry VARCHAR(20),
-    name VARCHAR(50),
-    phone VARCHAR(10),
+    firm_id NUMBER(6) NOT NULL,
+    industry VARCHAR(20) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
     PRIMARY KEY firm_id,
 )
 
 DROP TABLE services;
 CREATE TABLE services (
-    service_date DATE,
-    FOREIGN KEY firm_id REFERENCES contractor,
-    FOREIGN KEY claim_id REFERENCES claim,
+    service_date DATE NOT NULL,
+    FOREIGN KEY firm_id REFERENCES contractor NOT NULL,
+    FOREIGN KEY claim_id REFERENCES claim NOT NULL,
     PRIMARY KEY (firm_id, claim_id, service_date),
 )
 
 DROP TABLE claim;
 CREATE TABLE claim (
-    claim_id NUMBER(6),
-    claim_title VARCHAR(50),
-    location VARCHAR(100),
-    description VARCHAR(280),
-    occurred_date DATE,
-    submitted_date DATE,
-    FOREIGN KEY policy_id REFERENCES policy,
-    FOREIGN KEY cust_id REFERENCES customer,
+    claim_id NUMBER(6) NOT NULL,
+    claim_title VARCHAR(50) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    description VARCHAR(280) NOT NULL,
+    occurred_date DATE NOT NULL,
+    submitted_date DATE NOT NULL,
+    FOREIGN KEY policy_id REFERENCES policy NOT NULL,
+    FOREIGN KEY cust_id REFERENCES customer NOT NULL,
     PRIMARY KEY claim_id,
 )
 
 DROP TABLE manages;
 CREATE TABLE manages (
-    FOREIGN KEY claim_id REFERENCES claim,
-    FOREIGN KEY adj_id REFERENCES adjuster,
+    FOREIGN KEY claim_id REFERENCES claim NOT NULL,
+    FOREIGN KEY adj_id REFERENCES adjuster NOT NULL,
     PRIMARY KEY (claim_id, adj_id),
 )
 
 DROP TABLE agent;
 CREATE TABLE agent (
-    agent_id NUMBER(6),
-    name VARCHAR(100),
+    agent_id NUMBER(6) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     PRIMARY KEY agent_id,
 )
 
 DROP TABLE adjuster;
 CREATE TABLE adjuster (
-    adj_id NUMBER(6),
-    name VARCHAR(100),
+    adj_id NUMBER(6) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     specialty VARCHAR(20),
     PRIMARY KEY adj_id,
 )
 
 DROP TABLE dependent;
 CREATE TABLE dependent (
-    name VARCHAR (100),
-    relationship VARCHAR(20),
-    birth_date DATE,
-    FOREIGN KEY cust_id REFERENCES customer,
+    name VARCHAR (100) NOT NULL,
+    relationship VARCHAR(20) NOT NULL,
+    birth_date DATE NOT NULL,
+    FOREIGN KEY cust_id REFERENCES customer NOT NULL,
     PRIMARY KEY (name, cust_id),
 )
