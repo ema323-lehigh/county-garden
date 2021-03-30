fd9817ef0287a1fee172c53a82a38f0a2d7ff5fe
Emily Asadoorian <ema323@lehigh.edu>
2021-03-29 22:41:09 -0400

    Make conglomerating script

---
+++ data/copythis.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cat /dev/null > copythis.txt
+
+cat employees.txt >> copythis.txt
+echo -e '' >> copythis.txt
+cat agents.txt >> copythis.txt
+echo -e '' >> copythis.txt
+cat adjusters.txt >> copythis.txt
+echo -e '' >> copythis.txt
+cat customers.txt >> copythis.txt
+echo -e '' >> copythis.txt
