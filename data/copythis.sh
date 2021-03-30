c2602f185d2e180e0cee9a0c95579ac31aa6cead
Emily Asadoorian <ema323@lehigh.edu>
2021-03-29 22:42:46 -0400

    Add a preliminary clearing-out

--- data/copythis.sh
+++ data/copythis.sh
@@ -2,11 +2,15 @@
 
 cat /dev/null > copythis.txt
 
+echo "DELETE employee;" >> copythis.txt
 cat employees.txt >> copythis.txt
 echo -e '' >> copythis.txt
+echo "DELETE agent;" >> copythis.txt
 cat agents.txt >> copythis.txt
 echo -e '' >> copythis.txt
+echo "DELETE adjuster;" >> copythis.txt
 cat adjusters.txt >> copythis.txt
 echo -e '' >> copythis.txt
+echo "DELETE customer;" >> copythis.txt
 cat customers.txt >> copythis.txt
 echo -e '' >> copythis.txt
