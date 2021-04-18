e57c8ac6e623168db6237754ddc5b1f91c6a8ca8
Emily Asadoorian <ema323@lehigh.edu>
2021-04-17 22:51:55 -0400

    Throw in a Java file

---
+++ ema323/ema323.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ema323 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("welcome to dumbo town");
+        String dimbi = input.nextLine();
+        System.out.println("you said " + dimbi);
+        input.close();
+    }
+}
