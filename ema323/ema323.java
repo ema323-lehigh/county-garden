658e729f66d75cc06d98375b6cca067d3359bd1d
Emily Asadoorian <ema323@lehigh.edu>
2021-04-17 22:59:18 -0400

    Get a connection going

--- ema323/ema323.java
+++ ema323/ema323.java
@@ -1,3 +1,5 @@
+import java.sql.*;
+import java.io.*;
 import java.util.Scanner;
 
 public class ema323 {
@@ -6,6 +8,36 @@ public class ema323 {
         System.out.println("welcome to dumbo town");
         String dimbi = input.nextLine();
         System.out.println("you said " + dimbi);
-        input.close();
+
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("What's your Oracle username?");
+        String username = input.nextLine();
+        System.out.println("What's your Oracle password?");
+        String password = input.nextLine();
+        System.out.println("Connecting...");
+        String dbdeets = "jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241";
+
+        try (
+            Connection c = DriverManager.getConnection(dbdeets, username, password);
+            Statement s = c.createStatement();
+        ) {
+            System.out.println("...connected.");
+            System.out.println("--------------------------------------------------------------------------------");
+            c.setAutoCommit(false);
+            ResultSet r = s.executeQuery("SELECT * FROM customer");
+            while (r.next()) {
+                System.out.println(r.getString("firstname") + " " + r.getString("lastname"));
+            }     
+            s.close();
+            c.close();
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to connect or execute. Please try again later.");
+            if ((args.length > 0) && args[0].equals("-d")) { System.out.print(e); }
+        }
+        finally {
+            input.close();
+            System.out.println("--------------------------------------------------------------------------------");
+        }
     }
 }
