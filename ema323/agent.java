9ff67ce386b311ccacf3972696d06e533cbba5a9
Emily Asadoorian <ema323@lehigh.edu>
2021-05-07 03:12:37 -0400

    Let agents sign up new customers

--- ema323/agent.java
+++ ema323/agent.java
@@ -25,10 +25,10 @@ public class Agent {
                 boolean backout = false;
                 while (true) {
                     System.out.println("What would you like to do?");
-                    int choice = agentUtility.inputRequest(new String[] {"tell me a joke", "back"}, input);
+                    int choice = agentUtility.inputRequest(new String[] {"add a customer", "back"}, input);
                     switch (choice) {
                         case 1:
-                            System.out.println("A joke? You're a joke, trying to get into our systems.");
+                            addNewCustomer(c, input, agentID);
                             break;
                         case 2:
                             backout = true;
@@ -44,6 +44,48 @@ public class Agent {
                 System.out.println("Either way...scram! :)");
                 System.out.println("--------------------------------------------------------------------------------");
             }
+        }
+        catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    private static void addNewCustomer(Connection c, Scanner input, int agentID) throws SQLException {
+        try (Statement s = c.createStatement(); Statement t = c.createStatement();
+            PreparedStatement p = c.prepareStatement("INSERT INTO customer VALUES (?, ?, ?, ?, ?, ?, ?)");) {
+            Utility agentUtility = new Utility();
+            System.out.println("--------------------------------------------------------------------------------");
+            System.out.println("Enter the customer's first name:");
+            String fname = agentUtility.inputRequestString(input, "^\\D{1,50}$");
+            if (fname.equals("__BACK__")) { return; }
+            System.out.println("Enter the customer's middle initial (a single '-' for none):");
+            String minitial = agentUtility.inputRequestString(input, "^\\D|-$");
+            if (minitial.equals("__BACK__")) { return; }
+            if (minitial.equals("-")) { minitial = ""; }
+            System.out.println("Enter the customer's last name:");
+            String lname = agentUtility.inputRequestString(input, "^\\D{1,50}$");
+            if (lname.equals("__BACK__")) { return; }
+            System.out.println("Enter the customer's suffix/title (a single '-' for none):");
+            String suffix = agentUtility.inputRequestString(input, "^\\D{1,10}$");
+            if (suffix.equals("__BACK__")) { return; }
+            if (suffix.equals("-")) { minitial = ""; }
+            System.out.println("Enter the customer's date of birth (YYYY-MM-DD):");
+            String birthDate = agentUtility.inputRequestString(input, "^\\d{4}-\\d{2}-\\d{2}$");
+            if (birthDate.equals("__BACK__")) { return; }
+            p.setInt(1, new Random().nextInt(1000000)); p.setInt(7, agentID);
+            p.setDate(6, new java.sql.Date(0).valueOf(birthDate));
+            p.setString(2, fname); p.setString(3, minitial); p.setString(4, lname); p.setString(5, suffix);
+
+            try {
+                p.executeQuery();
+                c.commit();
+                System.out.println("Success! Your new happy customer has been signed up.");
+            }
+            catch (SQLException e) {
+                c.rollback();
+                System.out.println("Something seems to have gone wrong. Please try again soon.");
+            }
+            System.out.println("--------------------------------------------------------------------------------");
         }
         catch (SQLException e) {
             throw e;
