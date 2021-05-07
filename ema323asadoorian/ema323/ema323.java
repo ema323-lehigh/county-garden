7686b021d19281ba7d0283231a417692a1b8e5d1
Emily Asadoorian <ema323@lehigh.edu>
2021-05-07 14:22:35 -0400

    Add a welcome blurb

--- ema323asadoorian/ema323/ema323.java
+++ ema323asadoorian/ema323/ema323.java
@@ -6,6 +6,7 @@ import java.text.*;
 public class ema323 {
     public static void main(String[] args) {
         System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("Before we make any introductions, we need your login details !");
         Scanner input = new Scanner(System.in);
         System.out.println("What's your Oracle username?");
         String username = input.nextLine();
@@ -16,11 +17,22 @@ public class ema323 {
 
         try (Connection c = DriverManager.getConnection(dbdeets, username, password);) {
             System.out.println("...connected.");
-            System.out.println("(Please expand the height of your terminal window as much as possible!)");
+            System.out.println("(Please expand the width & height of your terminal window as much as possible!)");
             c.setAutoCommit(false);
             while (true) {
                 System.out.println("--------------------------------------------------------------------------------");
-                System.out.println("Welcome. Are you...which type of user in our system?");
+                System.out.println("Welcome to County Garden Insurance! We're thrilled to have you.");
+                System.out.println("Please find the navigation options as follows:");
+                System.out.println("- When presented with a menu with numbered bullet items, enter a numbered choice as such.");
+                System.out.println("- When presented with a freeform input, follow the indicated formatting (some more specific than others).");
+                System.out.println("- Most action menus will have a 'back' option to take you up to the last action menu seen.");
+                System.out.println("- Numbered menus presenting a data choice will not have this backout option.");
+                System.out.println("- When entering freeform input, the special code '__BACK__' will allow you to exit the entry action.");
+                System.out.println("- There are some numbered data menus that represent the go-ahead on a data update.");
+                System.out.println("- These will not have a back option of any kind, but don't sweat it! We can always re-up the database :).");
+                System.out.println("So, with all that said...!");
+                System.out.println("--------------------------------------------------------------------------------");
+                System.out.println("Are you...which type of user in our system?");
                 Utility mainUtility = new Utility(); // pull in object from shared functions class
                 int choice = mainUtility.inputRequest(new String[] {"agent", "adjuster", "customer"}, input);
                 switch (choice) {
